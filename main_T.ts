e7d5a622316ef9eea81924c542bef9f4e684a077
jiangbing <jiangbing@yuewen.com>
2020-09-27 17:52:42 +0800

    ts init

--- main_T.ts
+++ main_T.ts
@@ -1,3 +1,53 @@
 let a: string = 'data';
 
 console.log("a is string, value = " + a);
+
+
+class Member {
+    private _name: string;
+    private _age: number;
+    private _vip: boolean;
+
+
+    constructor(name: string, age: number, vip: boolean) {
+        this._name = name;
+        this._age = age;
+        this._vip = vip;
+    }
+
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    set age(value: number) {
+        this._age = value;
+    }
+
+    get vip(): boolean {
+        return this._vip;
+    }
+
+    set vip(value: boolean) {
+        this._vip = value;
+    }
+}
+
+const checkMember = ({id, info}: {id: number, info: Member }) => {
+    console.log('id = ' + id);
+    console.log('info = ' + info.name);
+};
+
+let param = {
+    id: 100,
+    info: new Member('bingbing', 10, false)
+}
+checkMember(param);
